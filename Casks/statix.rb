5c3fcedcd4b6679fafd3aad3b52ca2074c47bb28
statix-app <statix.mycomuapp@gmail.com>
2026-06-27 20:34:37 -0400

    version: 1.0.2
    version: 1.0.2

--- Casks/statix.rb
+++ Casks/statix.rb
@@ -1,21 +1,18 @@
 cask "statix" do
   on_arm do
-    version "1.0.1"
+    version "1.0.2"
     sha256 "11ad6dc84d5138f8aa6981f8403753c04e8e030a66930d8224818e6ca32c3cbd"
-
     url "https://statix.mycomuapp.com/software/#{version}/Statix-#{version}-arm64.dmg"
   end
   on_intel do
-    version "1.0.1"
+    version "1.0.2"
     sha256 "1c585076c86dd74dd748abbac2d6e2b8b51b2454ff2cd4213e3366242abb79ad"
-
     url "https://statix.mycomuapp.com/software/#{version}/Statix-#{version}.dmg"
   end
 
   name "Statix"
   desc "Sistema de análisis y estadísticas"
   homepage "https://statix.mycomuapp.com/"
-  icon "https://statix.mycomuapp.com/assets/img/logo.png"
 
   depends_on macos: :big_sur
 
